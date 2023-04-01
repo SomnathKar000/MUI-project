7589a674a1ac5c4e56bbf6489a2580d570325ec0
SomnathKar000 <somnathkar201901@gmail.com>
2023-04-01 21:23:49 +0530

    Add some css

--- src/components/Rightbar.js
+++ src/components/Rightbar.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const Rightbar = () => {
-  return <Box flex={2}>Right bar</Box>;
+  return (
+    <Box flex={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      Right bar
+    </Box>
+  );
 };
 
 export default Rightbar;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Box } from "@mui/material";
 
 const Sidebar = () => {
-  return <Box flex={1}>Side bar</Box>;
+  return (
+    <Box flex={1} sx={{ display: { xs: "none", sm: "block" } }}>
+      Side bar
+    </Box>
+  );
 };
 
 export default Sidebar;
